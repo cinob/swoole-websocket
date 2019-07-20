764e4c65e21117319b58bd25c8e4c3bb49be951b
cinob <cinob@foxmail.com>
2019-07-20 22:47:45 +0800

    add room

--- websocket.php
+++ websocket.php
@@ -1,6 +1,7 @@
 <?php
 $table = new Swoole\Table(1024);
 $table->column('fd', swoole_table::TYPE_STRING, 32);
+$table->column('rid', swoole_table::TYPE_INT, 10);
 $table->create();
 
 $server = new swoole_websocket_server('0.0.0.0', 8080);
@@ -8,34 +9,40 @@ $server->table = $table;
 
 //监听连接事件
 $server->on('open', function ($server, $request) {
-    $server->table->set($request->fd, ['fd'=>'用户'.$request->fd]);
-    pushToUser($server, '用户'.$request->fd . '加入房间');
+    // $server->table->set($request->fd, ['fd'=>'用户'.$request->fd]);
 });
 
 //监听接收消息事件
 $server->on('message', function ($server, $frame) {
     $data = json_decode($frame->data, true);
-    if ($data['fd'] > 0) {
-        $msg = [
-            '用户' . $frame->fd . '对你私聊说：' . $data['info'],
-            '你对用户'.$data['fd'].'私聊说：' . $data['info']
-        ];
-    } else {
-        $msg = '用户' . $frame->fd . '对大家说：' . $data['info'];
+    if (isset($data['name']) && isset($data['room'])) {
+        $server->table->set($frame->fd, ['fd' => $data['name'],'rid'=>$data['room']]);
+        pushToUser($server, '欢迎'.$data['name']. '进入本房间', $data['room']);
+    } else if (isset($data['fd'])) {
+        $user = $server->table->get($frame->fd);
+        if ($data['fd'] > 0) {
+            $msg = [
+                $user['name'] . '对你私聊说：' . $data['info'],
+                '你对'.$user['name'].'私聊说：' . $data['info']
+            ];
+        } else {
+            $msg = $user['name'] . '对大家说：' . $data['info'];
+        }
+        pushToUser($server, $msg, $user['rid'], $data['fd'], $frame->fd);
     }
-    pushToUser($server, $msg, $data['fd'], $frame->fd);
 });
 
 //监听关闭事件
 $server->on('close', function ($server, $fd) {
+    $user = $server->table->get($fd);
     $server->table->del($fd);
-    pushToUser($server, '用户'.$fd . '离开房间');
+    pushToUser($server, $user['name'] . '离开房间', $user['rid']);
 });
 
 //开启服务
 $server->start();
 
-function pushToUser($server, $msg, $userId = 0, $from = 0) {
+function pushToUser($server, $msg, $rid, $userId = 0, $from = 0) {
     if ($userId) {
         echo $msg[0] . PHP_EOL;//打印到我们终端
         echo $msg[1] . PHP_EOL;//打印到我们终端
@@ -45,10 +52,12 @@ function pushToUser($server, $msg, $userId = 0, $from = 0) {
         echo $msg . PHP_EOL;//打印到我们终端
         $users = [];
         foreach ($server->table as $fd => $v) {
-            $users[] = [
-                'fd' => $fd,
-                'name' => $v['fd']
-            ];
+            if ($v['rid'] == $rid) {
+                $users[] = [
+                    'fd' => $fd,
+                    'name' => $v['fd']
+                ];
+            }
         }
         foreach ($users as $v) {
             //将客户端发来的消息，推送给所有用户，也可以叫广播给所有在线客户端
